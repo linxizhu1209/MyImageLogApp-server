531f59ca3a7aa299655a2a824bc82111c5630e3d
linxizhu1209 <dlahj1209@naver.com>
2026-02-01 22:39:36 +0900

    초기 세팅

---
+++ src/main/java/com/imglog/myimagelogserver/MyImageLogServerApplication.java
@@ -0,0 +1,13 @@
+package com.imglog.myimagelogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyImageLogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyImageLogServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imglog/myimagelogserver/MyImageLogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imglog.myimagelogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyImageLogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
